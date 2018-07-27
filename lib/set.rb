42e4cac42dd27d37437f1301975040d58a706ceb
Dmitry <lbvf50.mobile@gmail.com>
2018-07-27 23:18:47 +0700

    Add check method for add new elements to set if it's need to

--- lib/set.rb
+++ lib/set.rb
@@ -9,5 +9,18 @@ module MissingNumbers
             @next_min = number - 1
             @next_max = number + 1
         end
+        def check(number)
+            if number == @next_max
+                @max +=1
+                @next_max +=1
+                return 1
+            end
+            if number == @next_min
+                @min -= 1
+                @next_min -= 1
+                return -1
+            end
+            nil
+        end
     end
 end
--- test/set_test.rb
+++ test/set_test.rb
@@ -17,4 +17,23 @@ describe 'Set' do
     assert_equal next_min, obj.next_min
     assert_equal next_max, obj.next_max
  end
+ it "If grow right it should return +1 and increate max and next max" do
+    obj = MissingNumbers::Set.new(5)
+    ans = obj.check(6)
+    assert_equal 1, ans
+    assert_equal 6, obj.max
+    assert_equal 7, obj.next_max
+ end
+ it "If grow left it should return -1 and dercease min and next_min" do
+    obj = MissingNumbers::Set.new(5)
+    ans = obj.check(4)
+    assert_equal(-1, ans)
+    assert_equal 4, obj.min
+    assert_equal 3, obj.next_min
+ end
+ it "Set dose not involve this number it return nil" do
+    obj = MissingNumbers::Set.new(5)
+    ans = obj.check(10)
+    assert_nil ans
+ end
 end
