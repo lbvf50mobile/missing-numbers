ad23dc7269149186ea33c414d7ed7643b1dd1d29
Dmitry <lbvf50.mobile@gmail.com>
2018-07-27 20:51:56 +0700

    Generator tests

--- lib/quest_generator.rb
+++ lib/quest_generator.rb
@@ -1,7 +1,15 @@
 module MissingNumbers
     class QuestGenerator
         def self.generate(input)
-            {}
+            max_value, missing_amonount = input[:max_value], input[:missing_amount]
+            quest = (1..max_value).to_a
+            answer = []
+            missing_amonount.times do
+                missing_value = quest.sample
+                answer.push(missing_value)
+                quest.delete(missing_value)
+            end
+            {quest: quest.shuffle, answer: answer.sort}
         end
     end
 end
--- test/quest_generator_test.rb
+++ test/quest_generator_test.rb
@@ -1,7 +1,20 @@
 require "./test/test_helper.rb"
 
 describe "QuestGenerator" do
+    before do
+        @max_value = rand(90) + 11
+        @missing_amount = rand(9) + 1
+        @input_hash = {max_value: @max_value, missing_amount: @missing_amount}
+        @output_hash = MissingNumbers::QuestGenerator.generate(@input_hash)
+    end
     it "Get hash return hash" do
-        assert_kind_of Hash, MissingNumbers::QuestGenerator.generate({max_value: 10, missing_amount: 2})
+        assert_kind_of Hash, MissingNumbers::QuestGenerator.generate(@input_hash)
+    end
+    it "Quest should have size  @max_value - @missing amount" do
+        assert_equal @max_value - @missing_amount, @output_hash[:quest].size
+    end
+    it "Answer should have size @missing_amount" do
+        @output_hash
+        assert_equal @missing_amount, @output_hash[:answer].size
     end
 end
