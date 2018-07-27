321a04ef75e5b220e115ccb3080afe6612c347d9
Dmitry <lbvf50.mobile@gmail.com>
2018-07-27 23:08:45 +0700

    Succesfully create Set

--- lib/set.rb
+++ lib/set.rb
@@ -2,7 +2,12 @@ module MissingNumbers
     class MaxValueSelectedError < ::RuntimeError
     end
     class Set
+        attr_reader :min, :max, :next_min, :next_max
         def initialize(number)
+            @min = number
+            @max = number
+            @next_min = number - 1
+            @next_max = number + 1
         end
     end
 end
--- test/set_test.rb
+++ test/set_test.rb
@@ -5,4 +5,16 @@ describe 'Set' do
   it 'Create with digit argument' do
     assert_kind_of Object, MissingNumbers::Set.new(5)
  end
+ it "After create it have min/max next_min/next max set as it should be" do
+    start = 5
+    min = start
+    max = start
+    next_min = min - 1
+    next_max = max + 1
+    obj = MissingNumbers::Set.new(start)
+    assert_equal min, obj.min
+    assert_equal max, obj.max
+    assert_equal next_min, obj.next_min
+    assert_equal next_max, obj.next_max
+ end
 end
