7cfe8a3f8cefbb3f75fe841e83b801e504f9df88
Dmitry <lbvf50.mobile@gmail.com>
2018-07-31 02:33:10 +0700

    DisjoingArraySolve past first tests

--- lib/disjoint_array_solve.rb
+++ lib/disjoint_array_solve.rb
@@ -4,7 +4,11 @@ require './lib/storage.rb'
 module MissingNumbers
     class DisjointArraySolve
         def self.miss_nums_finder(arr)
-
+          storage = MissingNumbers::Storage.new
+          arr.each do |number|
+            storage.insert(number)
+          end
+          storage.gaps
         end
     end
 end
