d302ac2d24a7faffc0e57f7ddb1b23951ec0f6b8
Dmitry <lbvf50.mobile@gmail.com>
2018-07-31 15:00:03 +0700

    VectorBoolSolve past initial test

--- lib/vector_bool_solve.rb
+++ lib/vector_bool_solve.rb
@@ -1,19 +1,17 @@
 module MissingNumbers
     class VectorBoolSolve
         def self.miss_nums_finder(arr)
-            arr.sort!
-            max = arr.max
-            a = arr.each
-            ans = []
-            (1..max).each do |v|
-              x = a.peek
-              if v == x
-                a.next
-              else
-                ans << v
-              end
-            end
-            return ans
+          max = arr.max
+          checked_missed_values = Array.new(max+1,true)
+          checked_missed_values[0] = false # Numbers start from 1, not for 0
+          ans = []
+          arr.each do |x|
+            checked_missed_values[x] = false
+          end
+          checked_missed_values.each_with_index do |missed_value,index|
+            ans << index if missed_value
+          end
+           ans
         end
     end
 end
