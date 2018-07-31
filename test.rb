c9c42131ede75a7c9e4e70d4eb869768ad0146a8
Dmitry <lbvf50.mobile@gmail.com>
2018-07-31 15:29:06 +0700

    test.rb set solutions in array

--- test.rb
+++ test.rb
@@ -1,24 +1,26 @@
 Dir["./test/*.rb"].each {|file| require file }
 
+Solutions = [ 
+    MissingNumbers::BasicSolve, 
+    MissingNumbers::DisjointArraySolve,
+    MissingNumbers::VectorBoolSolve ]
+
+def test_solutions(task:)
+    Solutions.each do |solutions_class|
+        ans = solutions_class.miss_nums_finder(task[:quest])
+        assert_equal ans.sort, task[:answer]
+    end
+end
+
 describe "Test BasicSove with QuestGenerator" do
     it "Test on 5 elements" do
         task = MissingNumbers::QuestGenerator.generate({max_value:5, missing_amount:1})
-        ans = MissingNumbers::BasicSolve.miss_nums_finder(task[:quest])
-        ans_disjoint = MissingNumbers::DisjointArraySolve.miss_nums_finder(task[:quest])
-        ans_vector = MissingNumbers::VectorBoolSolve.miss_nums_finder(task[:quest])
-        assert_equal ans.sort, task[:answer]
-        assert_equal ans_disjoint.sort, task[:answer]
-        assert_equal ans_vector.sort, task[:answer]
+        test_solutions(task: task)
     end
     it "Test on RAnd elements" do
         @max_value = rand(90) + 11
         @missing_amount = rand(9) + 1
         task = MissingNumbers::QuestGenerator.generate({max_value:@max_value, missing_amount:@missing_amount})
-        ans = MissingNumbers::BasicSolve.miss_nums_finder(task[:quest])
-        ans_disjoint = MissingNumbers::DisjointArraySolve.miss_nums_finder(task[:quest])
-        ans_vector = MissingNumbers::VectorBoolSolve.miss_nums_finder(task[:quest])
-        assert_equal ans.sort, task[:answer]
-        assert_equal ans_disjoint.sort, task[:answer]
-        assert_equal ans_vector.sort, task[:answer]
+        test_solutions(task: task)
     end
 end
